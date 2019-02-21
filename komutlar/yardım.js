0da882a326fb9c3ad9d5806399ee0abec5433d12
King Strafers <42972814+denemebot12@users.noreply.github.com>
2019-02-21 18:40:47 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -9,10 +9,9 @@ exports.run = (client, message, params) => {
   .setDescription('')
   .setColor(0x00ffff)
   .addField("**Eğlence ve Kullanıcı Komutları:**", `..banned = Dene ve Gör! \n..avatarım = Avatarınınızı Gösterir. \n..herkesebendençay = Herkese Çay Alırsınız. \n..koş = Koşarsınız.\n..çayiç = Çay İçersiniz. \n..çekiç = İstediğiniz Kişiye Çekiç Atarsınız. \n..çayaşekerat = Çaya Şeker Atarsınız. \n..yumruh-at = Yumruk Atarsınız. \n..yaz = Bota İstediğiniz Şeyi Yazdırırsınız. \n..sunucuresmi = BOT Sunucunun Resmini Atar. \n..sunucubilgi = BOT Sunucu Hakkında Bilgi Verir. \n-kullanıcıbilgim = Sizin Hakkınızda Bilgi Verir. `)
-  .addField("**Sunucu Yetkilisi Komutları**", `-ban = İstediğiniz Kişiyi Sunucudan Banlar. \n-kick  = İstediğiniz Kişiyi Sunucudan Atar. \n-unban = İstediğiniz Kişinin Yasağını Açar. \n-sustur = İstediğiniz Kişiyi Susturur. -MUTED- \n-oylama = Oylama Açar. \n-duyuru = Güzel Bir Duyuru Görünümü Sağlar.`)
-  .addField("**Botun Ana Komutları**", "-yardım = BOT Komutlarını Atar. \n-bilgi = BOT Kendisi Hakkında Bilgi Verir. \n-ping = BOT Gecikme Süresini Söyler. \n-davet = BOT Davet Linkini Atar. \n-istatistik = BOT İstatistiklerini Atar.")
-  .addField("**Yapımcı Komutları:**", " **Güvenlik Amacı İle Yapımcı Komutları Gösterilmiyor** ")
-  .setFooter('**Alt yapıdır.**')
+  .addField("**Sunucu Yetkilisi Komutları**", `..ban = İstediğiniz Kişiyi Sunucudan Banlar. \n..kick  = İstediğiniz Kişiyi Sunucudan Atar. \n..unban = İstediğiniz Kişinin Yasağını Açar. \n..sustur = İstediğiniz Kişiyi Susturur. -MUTED- \n..oylama = Oylama Açar. \n-duyuru = Güzel Bir Duyuru Görünümü Sağlar.`)
+  .addField("**Botun Ana Komutları**", "..yardım = BOT Komutlarını Atar. \n..bilgi = BOT Kendisi Hakkında Bilgi Verir. \n..ping = BOT Gecikme Süresini Söyler. \n..davet = BOT Davet Linkini Atar. \n..istatistik = BOT İstatistiklerini Atar.")
+  .addField ("**Yapımcı**:Kafkas.exe#7264
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
